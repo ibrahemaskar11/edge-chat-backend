9a0e79b95add0a601c4eca62e1ac31bf95f8c382
ibrahim <ibrahemhani2014@gmail.com>
2023-03-22 01:36:05 +0200

    tryout

--- server.ts
+++ server.ts
@@ -12,6 +12,7 @@ const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+
 // process.on("unhandledRejection", (err) => {
 //   console.log("UNHANDLED REJECTION! 💥 Shutting down...");
 //   console.log(err.name, err.message);
